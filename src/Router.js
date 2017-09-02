25e00a0305209eeb0cb70b2da114205b18db6f2e
Fareez Ahmed <fareez1234@gmail.com>
2017-09-02 12:02:51 +1000

    Navigate Bar Button Completed

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="login" component={LoginForm} title="Please Login" />
-      <Scene key="employeeList" component={EmployeeList} title="List" />
+      <Scene key="main">
+        <Scene
+          onRight={() => Actions.employeeCreate()}
+          rightTitle="Add" 
+          key="employeeList" 
+          component={EmployeeList} 
+          title="List" 
+        />
+        <Scene 
+          key="employeeCreate"
+          title="Create Employee"
+          component={EmployeeCreate}
+        />
+      </Scene>
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Scene>
     </Router>
   );
 };
---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+// import { Card, CardSection } from './common';
+
+class EmployeeCreate extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Employee Create</Text>
+            </View>
+        );
+    }
+}
+
+export default EmployeeCreate;
